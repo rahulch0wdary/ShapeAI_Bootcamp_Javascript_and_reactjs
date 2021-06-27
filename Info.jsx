2467b2bc4c1f5d3b84f667b0276a94479f3475b3
rahulch0wdary <69949876+rahulch0wdary@users.noreply.github.com>
2021-06-27 15:35:06 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Captain America</h1>
+      <p>
+        The First Avenger and fought with the Hydra(The Fictional Terrorist
+        Organization)
+      </p>
+    </div>
+  );
+}
+
+export default Info;
